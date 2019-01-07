a19232e8d0609c8d36c04af2fcbcd0789ef5ae3f
= <JeremieBou>
2019-01-06 21:18:38 -0400

    fixed panlex

--- src/data/make_panlex.py
+++ src/data/make_panlex.py
@@ -1,13 +1,14 @@
 import time
 import sys
+import json
 
-from requests import get
+from requests import get, post
 
 URL = 'http://api.panlex.org/v2/'
-RATE_LIMIT = 120
+RATE_LIMIT = 100
 REQ_LIMIT = 2000 # how many things can be returned by request
 REQ_LEN_LIMIT = 400 # basically if I put an array that's bigger then this in the input I'll get a 414 error
-
+OFFSET_LIMIT = 125000
 
 req_counter = 0
 
@@ -23,8 +24,8 @@ class Lexicon():
         self.out_count = 0
 
         self.lang_ids = {
-            in_id: in_lang,
-            out_id: out_lang
+            self.in_id: self.in_lang,
+            self.out_id: self.out_lang
         }
 
         self.lexicon = {}
@@ -34,7 +35,7 @@ class Lexicon():
 
         if lang:
             if expression.get('trans_expr'):
-                if lang == self.in_lang
+                if lang == self.in_lang:
                     expr = expression['trans_expr']
                 else:
                     expr = expression['id']
@@ -46,8 +47,8 @@ class Lexicon():
 
             if not self.lexicon.get(expr):
                 self.lexicon[expr] = {
-                    self.in_lang = set(),
-                    self.out_lang = set()
+                    self.in_lang: set(),
+                    self.out_lang: set()
                 }
 
             if lang == self.in_lang:
@@ -58,17 +59,17 @@ class Lexicon():
                 self.out_count += 1
 
     def add_expressions(self, expressions):
-        for expression in expression:
+        for expression in expressions:
             self.add_expression(expression)
 
     def make_output(self):
         output = []
-        for key, value in self.lexicon:
+        for key, value in self.lexicon.items():
             if len(value[self.out_lang]) > 0:
                 if len(value[self.in_lang]) > 0:
-                    for in in value[self.in_lang]:
+                    for inp in value[self.in_lang]:
                         for out in value[self.out_lang]:
-                            output.append((in, out))
+                            output.append((inp, out))
                 else:
                     for out in value[self.out_lang]:
                         output.append(("<UNK>", out))
@@ -86,14 +87,19 @@ class Lexicon():
 
 
 
-def rurl(req, params=None):
-    #If we're reaching the rate limit, pause
-    if req_counter == RATE_LIMIT - 5:
-        time.sleep(65)
-        req_counter == 0
-    req_counter += 1
+def rurl(req, params=None, long=False):
+    time.sleep(0.5)
+    if long:
+        a = post(URL + req, data=json.dumps(params))
+    else:
+        a = get(URL + req, params=params)
 
-    a = get(URL + req, params=params).json()
+    if a.status_code != 200:
+        print(a)
+        print(a.text)
+        return
+
+    a = a.json()
 
     try:
         return a['result']
@@ -103,23 +109,22 @@ def rurl(req, params=None):
 
 def get_langvar_id(lang):
     lang_var = rurl('langvar', params = {'uid': lang})
-    return lang_var['id']
+    return lang_var[0]['id']
 
 def get_expressions(lang):
     i = 0
     expressions = []
 
     while True:
-        print(i)
         new_exprs = rurl('expr',
                          params={'uid': lang, 'offset': REQ_LIMIT*i})
 
-        if len(new_exprs) == 0 or i > 125:
+        i += 1
+        if len(new_exprs) == 0 or REQ_LIMIT*i > OFFSET_LIMIT or i > 10:
             break
         else:
             expressions += new_exprs
 
-        i += 1
 
     return expressions
 
@@ -128,34 +133,31 @@ def get_ids(expressions, lang):
     for expression in expressions:
         ids.append(expression['id'])
 
-    return ids, lexicon
+    return ids
 
-def translate(expressions, target):
+def translate(ids, target):
     out = []
-    for i in range(0, len(expressions), REQ_LEN_LIMIT):
-        eng = rurl('expr',
+    for i in range(0, len(ids), REQ_LIMIT):
+        resp = rurl('expr',
                    params={'uid': target,
-                           "trans_expr": expressions[i:i+REQ_LIMIT]})
-        out = out + eng
+                           'trans_expr': ids[i:i+REQ_LIMIT]},
+                    long=True)
+
+        out = out + resp
 
     return out
 
 
 def make_lexicon(in_lang, out_lang):
     lexicon = Lexicon(in_lang, out_lang)
-
     in_expressions = get_expressions(in_lang)
     out_expressions = get_expressions(out_lang)
-
     lexicon.add_expressions(in_expressions)
     lexicon.add_expressions(out_expressions)
-
     in_ids = get_ids(in_expressions, in_lang)
     out_ids = get_ids(out_expressions, out_lang)
-
     in_to_out = translate(in_ids, out_lang)
     out_to_in = translate(out_ids, in_lang)
-
     lexicon.add_expressions(in_to_out)
     lexicon.add_expressions(out_to_in)
 
@@ -163,13 +165,13 @@ def make_lexicon(in_lang, out_lang):
 
 
 if __name__ == "__main__":
-    in_lang = argv[1]
-    out_lang = argv[2]
+    in_lang = sys.argv[1]
+    out_lang = sys.argv[2]
 
     if len(in_lang) == len(out_lang) == 7:
         lexicon = make_lexicon(in_lang, out_lang)
         lexicon.print_output()
-    else
+    else:
         print("""use language format XXX_000, were XXX is the alpha-3 code,
                 and 000 is the language variety(use 000 if you don't know what
                 the variety is)""")
